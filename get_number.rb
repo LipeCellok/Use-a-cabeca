c0bda16492e758241810b464689e0f863787ddee
Felipe Espíndola <you@example.com>
2023-04-17 15:32:22 -0300

    Pagina 26

--- get_number.rb
+++ get_number.rb
@@ -2,10 +2,20 @@
 #Escrito por: Felipe Espíndola
 
 puts "Bem Vindo ao 'Get My Number!'"
+
+#Obtem o nome do jogador
 print "Qual o seu nome?"
 input = gets
 puts "Bem Vindo, #{input}"
 
+#Armazena numero aleatorio para ser adivinhado
 puts "Eu tenho um numero aleatorio de 1 a 100"
 puts "Você consegue adivinhar?"
 target = rand(100)+1
+
+#Verifica as tentativas realizadas pelo jogador
+num_guess = 0
+remaining_guesses = 10 
+puts "Você tem #{remaining_guesses - num_guess} tentativas restantes."
+print "Vamos a sua tentativa: "
+guess = gets.to_i
