89af9723056e64f5db9feb7b568e25bfbbba2264
Felipe Espíndola <you@example.com>
2023-04-25 15:57:07 -0300

    Dia 25/04

--- vehicle_methods.rb
+++ vehicle_methods.rb
@@ -14,10 +14,16 @@ def use_headlights (brightness = "low-beam")
 end
 
 def mileage(miles_driven, gas_used)
-    return miles_driven / gas_used
+    if gas_used == 0
+        return 0.0
+    end
+    miles_driven / gas_used
 end
 
-trip_mileage = mileage(400, 12)
-puts "You got #{trip_mileage} MPG on this trip"
+#trip_mileage = mileage(400, 12)
+#puts "You got #{trip_mileage} MPG on this trip"
 
-lifetime_mileage
+#lifetime_mileage = mileage(11432, 366)
+#puts "This car averages #{lifetime_mileage} MPG"
+
+puts mileage(0, 0)
